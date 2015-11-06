05cc34ab492f9deb678ae13745225a53ba1ee0e6
Frank Breedijk <fbreedijk@schubergphilis.com>
2015-11-06 15:57:22 +0100

    Copyright fixes

--- SeccubusV2/SeccubusHostnames.pm
+++ SeccubusV2/SeccubusHostnames.pm
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright 2014 Frank Breedijk, Alex Smirnoff
+# Copyright 2015 Frank Breedijk, Alex Smirnoff, Michael Dopheide
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
